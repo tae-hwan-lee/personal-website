44e225b9925212cec9996042517f671e01a3d685
otaeotay <15leetaehwan@gmail.com>
2023-12-05 20:59:26 -0800

    feat: under construction

--- src/App.js
+++ src/App.js
@@ -1,53 +1,89 @@
 import React from 'react';
 import './App.css';
-import Intro from './Components/Intro';
-import Logos from './Components/Logos';
-import About from './Components/About';
-import Footer from './Components/Footer';
-import Projects from './Components/Projects';
-import Contact from './Components/Contact';
-import { Nav, Navbar } from 'react-bootstrap';
-import TL from './images/TL.svg';
-import 'animate.css/animate.min.css';
+import linkedin from './images/underconstruction/linkedin.png';
+import github from './images/underconstruction/github.png';
 
 function App() {
+  console.log(`
+  what are you doing here...
+                                    ,,
+                             _,g╣╢Ñ╨╜╜╨╩╣@╖
+                           ,@╣Ñ╜_        __╙╢H,
+                         ╓╬╢╜_               _╙╣╖
+                        ╫╢╜_                    \`╫,
+                       ╣╢                         ╙%_
+                      ╢╢                            ╙,,,,╖╖╥╖╥╥m╥╖,
+                     ]╢[  ,         _ ,,╓,╓╖╥@@@╣╣╢╣╣Ñ╝╜╜╜╙╙╙"╙╙╙╜╜_
+                      ╢╢╓▒▒╖m@@@ÑÑ╝╝╝╜╜╙╙"\` _____   ▒_
+                       ║╢░__                         ▒
+                      ]▒▒       ,              ╒▄    ╙_
+                      ▒▒_       ▀▀              __    ▒
+                      ▒▒                              ▒░
+                      ▒▒_    _                      ╓@_▒
+                      '▒▒     *╖                  ▄█▀__▒
+                       _▒▒.     ╙▀▄,        _,▄▄█▀\`_  ]▒
+                         ╙▒▒╖      _\`╙▀▀▀Ñ▀▀▀▀ __     ▒▒
+                           _╙▒▒h,                    ▒▒_
+                             @_\`╜▒▒╖╖._            ╓▒▒_
+                            ]H    _\`╙╜▒▒▒∩╖╖╓╓╓╖H▒▒╜▒
+                            ╟H          _ \`"╙"\`\`    ╢_
+                            ╫_                      ╫_
+                            ╣                       ╢
+                           ]▓    ╫             ╢   _╣
+                           ╟▌    ╣             ╫_  ]╣_
+                           ╫[   j╣             ╫   ]╣_
+                           ╫L   ]▓             ╫_  ╟▓_
+                           ╫U   ╟▓             ╫H  ╟╣_
+                          ]╣_   ╫▌             ╫[  ╟╢_
+                          ]▓    ▓[             ╫H  ╟▓_
+                          ]╣_   ╫[             ╢@,,▓╩
+                           ▓▓ ,,╣[            _╫[_\`_
+                           _╙╙╙_╣[            _▓@
+                               j╣[              ∩
+                               ]╣L       ]      [
+                                ╙        ╟[     [
+                                ╟_       ║[    ][
+                                ╫_       ║╝    ][
+                                ╢_       ]▒    ╟[
+                                ▓        ╟╣    ╟C
+                                ▓_       ╟╣    ╫L
+                               ]▓_       ╟[    ▓C
+                               ▐▓        ╫H    ▓U
+                               ╟[        ▓_   _▓
+                               ▓[       ]▓_    ▓_
+                              j▓_       ╟▌     ▓∩
+                              ╙╣        ╫▌    ]╢C
+                               ▐,       ╓▄     ▀█▄
+                                ▀▄       ▀█▄    _██_
+                                 ╙▓,       ▀█,    ▀█
+                                   ▀█,      ╙█▄    ▓▌
+                                    _▀█,     _██N▄,▓▀
+                                       ▀█▄    j█____
+                                         \`▀██▄█▀_
+                                             _
+  `);
+
   return (
-    <div className="App">
-      <Navbar
-        bg="main"
-        variant="light"
-        fixed="top"
-        expand="sm"
-        collapseOnSelect
-        className="nav-container"
+    <div className='App'>
+      <a
+        href='https://www.linkedin.com/in/tae-hwan-lee/'
+        target='_blank'
+        rel='noopener noreferrer'
       >
-        <Navbar.Brand>
-          <a href="#intro">
-            <img src={TL} alt="Logo" id="TL"></img>
-          </a>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav>
-            <Nav.Link href="#about" id="nav-about">
-              ABOUT
-            </Nav.Link>
-            <Nav.Link href="#projects" id="nav-projects">
-              PROJECTS
-            </Nav.Link>
-            <Nav.Link href="#contact" id="nav-contact">
-              CONTACT
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <hr></hr>
-      <Intro></Intro>
-      <Logos></Logos>
-      <About></About>
-      <Projects></Projects>
-      <Contact></Contact>
-      <Footer></Footer>
+        <img
+          src={linkedin}
+          alt='LinkedIn'
+          className='logos'
+          id='linked-in'
+        ></img>
+      </a>
+      <a
+        href='https://github.com/tae-hwan-lee'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img src={github} alt='Github' className='logos' id='github'></img>
+      </a>
     </div>
   );
 }
