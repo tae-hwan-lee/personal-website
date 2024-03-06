4295e7ea11883042ea457f164bb8f32adbad521b
otaeotay <15leetaehwan@gmail.com>
2024-03-05 19:39:50 -0800

    chore: move console guy to a util

--- src/App.js
+++ src/App.js
@@ -1,67 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import linkedin from './images/underconstruction/linkedin.png';
 import github from './images/underconstruction/github.png';
+import epicConsoleLogger from './utils/epicConsoleLogger';
 
 function App() {
-  console.log(`
-  what are you doing here...
-                                    ,,
-                             _,g╣╢Ñ╨╜╜╨╩╣@╖
-                           ,@╣Ñ╜_        __╙╢H,
-                         ╓╬╢╜_               _╙╣╖
-                        ╫╢╜_                    \`╫,
-                       ╣╢                         ╙%_
-                      ╢╢                            ╙,,,,╖╖╥╖╥╥m╥╖,
-                     ]╢[  ,         _ ,,╓,╓╖╥@@@╣╣╢╣╣Ñ╝╜╜╜╙╙╙"╙╙╙╜╜_
-                      ╢╢╓▒▒╖m@@@ÑÑ╝╝╝╜╜╙╙"\` _____   ▒_
-                       ║╢░__                         ▒
-                      ]▒▒       ,              ╒▄    ╙_
-                      ▒▒_       ▀▀              __    ▒
-                      ▒▒                              ▒░
-                      ▒▒_    _                      ╓@_▒
-                      '▒▒     *╖                  ▄█▀__▒
-                       _▒▒.     ╙▀▄,        _,▄▄█▀\`_  ]▒
-                         ╙▒▒╖      _\`╙▀▀▀Ñ▀▀▀▀ __     ▒▒
-                           _╙▒▒h,                    ▒▒_
-                             @_\`╜▒▒╖╖._            ╓▒▒_
-                            ]H    _\`╙╜▒▒▒∩╖╖╓╓╓╖H▒▒╜▒
-                            ╟H          _ \`"╙"\`\`    ╢_
-                            ╫_                      ╫_
-                            ╣                       ╢
-                           ]▓    ╫             ╢   _╣
-                           ╟▌    ╣             ╫_  ]╣_
-                           ╫[   j╣             ╫   ]╣_
-                           ╫L   ]▓             ╫_  ╟▓_
-                           ╫U   ╟▓             ╫H  ╟╣_
-                          ]╣_   ╫▌             ╫[  ╟╢_
-                          ]▓    ▓[             ╫H  ╟▓_
-                          ]╣_   ╫[             ╢@,,▓╩
-                           ▓▓ ,,╣[            _╫[_\`_
-                           _╙╙╙_╣[            _▓@
-                               j╣[              ∩
-                               ]╣L       ]      [
-                                ╙        ╟[     [
-                                ╟_       ║[    ][
-                                ╫_       ║╝    ][
-                                ╢_       ]▒    ╟[
-                                ▓        ╟╣    ╟C
-                                ▓_       ╟╣    ╫L
-                               ]▓_       ╟[    ▓C
-                               ▐▓        ╫H    ▓U
-                               ╟[        ▓_   _▓
-                               ▓[       ]▓_    ▓_
-                              j▓_       ╟▌     ▓∩
-                              ╙╣        ╫▌    ]╢C
-                               ▐,       ╓▄     ▀█▄
-                                ▀▄       ▀█▄    _██_
-                                 ╙▓,       ▀█,    ▀█
-                                   ▀█,      ╙█▄    ▓▌
-                                    _▀█,     _██N▄,▓▀
-                                       ▀█▄    j█____
-                                         \`▀██▄█▀_
-                                             _
-  `);
+  useEffect(() => {
+    epicConsoleLogger();
+  }, []);
 
   return (
     <div className='App'>
