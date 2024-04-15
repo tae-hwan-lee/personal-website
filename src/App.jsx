9cbfde814ad07fc0abbc7b72e21b4e1c35b92cd8
otaeotay <15leetaehwan@gmail.com>
2024-04-14 21:05:00 -0700

    chore: clean up old css and import

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import useThemeStore from './utils/zustand/useThemeStore.js';
 import { Header } from './components/Header';
-import { Home } from './pages/home';
+import { Home } from './pages/Home';
 import { Footer } from './components/Footer';
 
 function App() {
