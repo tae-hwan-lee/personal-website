61c86572c4e76a2a6b5c11e0caf3bfa15917f903
otaeotay <15leetaehwan@gmail.com>
2024-07-01 18:14:05 -0700

    chore: remove about stuff from main page lolz!

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -49,9 +49,8 @@ export const Home = () => {
     <main>
       <div
         id='intro'
-        className='flex justify-center items-center h-almost-screen'
+        className='flex justify-center items-center h-almost-screen overflow-hidden'
       >
-        {/* <div className=''>art</div> */}
         <motion.div
           drag
           dragConstraints={constraints}
@@ -76,29 +75,6 @@ export const Home = () => {
             onClick={handleThemeChange}
           ></img>
         </motion.div>
-      </div>
-      <div className='w-4/5 border-b right-0'></div>
-      <div className='my-20 ml-48 flex flex-col gap-12'>
-        <div>
-          <p>✩ō͡≡o</p>
-          <p>smartcar</p>
-          <p>2022-present</p>
-          <p>software engineer</p>
-          <p>react/node</p>
-        </div>
-        <div>
-          <p>(⌐▀͡ ̯ʖ▀)</p>
-          <p>critchfield mechanical, inc.</p>
-          <p>2019-2021</p>
-          <p>mechanical engineer</p>
-          <p>designed hvac systems for commercial buildings</p>
-        </div>
-        <div>
-          <p>૮ ・ﻌ・ა</p>
-          <p>cal poly, slo</p>
-          <p>2015-2019</p>
-          <p>mechanical engineering student</p>
-        </div>
       </div>
     </main>
   );
